1449013b110d3c4ab52fbd700b48a9cad6ed12ee
duanyytop <duanyytop@gmail.com>
2020-12-24 16:16:23 +0800

    chore: Update hash signature verifying

--- src/main/java/dev/gw/Main.java
+++ src/main/java/dev/gw/Main.java
@@ -22,6 +22,16 @@ public class Main {
 
         Boolean result = verifySignature(publicKey, Utils.hexStringToByteArray(message), Utils.hexStringToByteArray(signature));
         System.out.println("Signature verify result: " + result);
+
+
+        message = "aad8cc3d08805be9a40b406a7b91f0024b344eb9097aff347dc179055668c69d";
+        signature = "0d6f2acf87d0347fb8392fd1e6aa2006cc2ab58a4a5dac928ab57e6a741a676a10a120d71f6fecad067be2fdb3d07178579b9257c943195f7f49dd0e3f66891686971bce2d55b50827148a3b86b5cb20ea507895c7c899e3b010fe8ccae5f1b0dad3c7e0f0dd41414dc50b04a693139251cadc8e8cd6fc1e2111f75bf53ae9f17d8dc9e5e0b9549a53a4238cc4d12c07ad67f8324297809af9517faaca55c6a97e470e60f898094874679a2a8f2ed3f3559e56ddf9e7d86a911f90eb290f1401ae8ed50205df7518df011463ec6660ce6950421c3825f89200c6c39f8c5b4f975dedd75caa24d4807788707b10884a8bed168b05e5f645d59ee7a975f2a7c94944c4934b86c75a4c5662e6e3da82061b7aa09cc4c7b099dec76f84b12e4a160740f7e843f888b7fcfd56425b8c3da52ddbbbc491078935e241c148b724a0b00380d8be2fbfbe575fdbcef7641671021d07eebb9ac90be4105caadaa9c0bb899a30687bb9ea7fd3062aff58e06fdf750f589c74fd4115cf32bc19304e57d9c44865e5c38d9dda14c52b867a5d3b278d7a7df2b5444c97f3e2dedf36a4eab6d1c10e86a78220b8067ea883340a0204ee4e2aee465e972ded4591090812c704a0877b5888c567dea759b26406fb29210e1ecda1ca2a4fc2b0a7811764e70d17419ae0cda39312daedd252e6de4690d800edaee60d0d0445ead352b9089a039036da";
+        for (int i = 0; i < 4; i++) {
+            String subMessage = message.substring(16 * i, 16 * (i + 1));
+            String subSignature = signature.substring(256 * i, 256 * (i + 1));
+            result = verifySignature(publicKey, Utils.hexStringToByteArray(subMessage), Utils.hexStringToByteArray(subSignature));
+            System.out.println("Multi signature verify result: " + result);
+        }
     }
 
     static Boolean verifySignature(PublicKey pubKey, byte[] origin, byte[] signature) throws Exception {
